b355fe671d11c701be0fd103c0ea6a3c8cd74b24
Thiago David <thiagodavidpd@gmail.com>
2021-07-09 15:04:03 -0300

    :lipstick: CSS of boardgame

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import CardGame from "./src/components/CardGame";
 import './src/styles/settings/colors.css'
-import './src/styles/elements/base.css'
 import './src/styles/generic/reset.css'
 import BoardGame from "./src/objects/BoardGame";
+import './src/styles/elements/base.css'
 
 const $root = document.querySelector("#root");
 const $htmlBoardGame = BoardGame(6);
