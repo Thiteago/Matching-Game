0c8861d1adf6d2aedc4d53f9b8ccec91215cc9b1
Thiago David <thiagodavidpd@gmail.com>
2021-07-08 23:08:15 -0300

    :sparkles: Added alt logo

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 function CardGame() {
     return `
         <article class="card-game">
-            <img src="images/alura-pixel.svg">
+            <img src="images/alura-pixel.svg" alt="logo da alura">
         </article>
     `
 }
