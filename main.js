aa0de4876184f81ca31cb0fb3fb60369b1861643
Thiago David <thiagodavidpd@gmail.com>
2021-07-14 20:26:17 -0300

    :art: create CardFrontBack component

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
-import BoardGame from "./src/objects/BoardGame";
 import './src/styles/elements/base.css'
 import ScoreBoard from './src/objects/ScoreBoard';
+import BoardGame from './src/objects/BoardGame'
 
 const $root = document.querySelector("#root");
 
@@ -10,6 +10,6 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML("beforeend",
 `
     ${ScoreBoard()}
-    ${BoardGame(6)}
+    ${BoardGame(2)}
 
 `);
