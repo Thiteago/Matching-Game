52e1b1a23414dbe57bb6d4d3b98a14be6a0366d5
Thiago David <thiagodavidpd@gmail.com>
2021-07-14 21:56:21 -0300

    :lipstick: create css and animation of flip card

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-function CardGame(icon = 'alura-pixel', alt = 'Logo da Alura') {
+function CardGame(icon = "alura-pixel", alt = "Logo da Alura") {
     return /*html*/`
         <article class="card-game">
             <img src="images/${icon}.svg" alt="${alt}">
